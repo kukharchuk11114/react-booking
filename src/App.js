f0eb24fd6cb0ef11e28deaa0c0b86422436625bc
kukharchuk11114 <kukharchukillia739@gmail.com>
2024-05-27 23:55:37 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
       <Offers />
       <About />
       <Blog />
-      {/* <Footer /> */}
+      <Footer />
     </>)
 }
 
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
-import './footer.css'
+import './footer.scss'
+import { GiPlanetConquest } from "react-icons/gi";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className="secContainer container grid">
+        <div className="logoDiv">
+          <div className="footerLogo">
+            <a href="#" className='logo flex'>
+            <h1 className='flex'> 
+            <GiPlanetConquest className='icon'/>
+            Lil Co.
+            </h1>
+            </a>
+          </div>
+
+          <div className="socials flex">
+          <FaFacebook className='icon'/>
+          <FaTwitter className='icon'/>
+          <FaInstagramSquare className='icon'/>
+          </div>
+        </div>  
+
+        <div className="footerLinks">
+          <span className="linkTitle">
+            Informations
+          </span>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Explore</a>
+          </li>
+          <li>
+            <a href="#">Travel </a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+        </div>
+        <div className="footerLinks">
+          <span className="linkTitle">
+            Contact Us
+          </span>
+          <span className="phone">+48 444 444 444</span>
+          <span className="email">lilkuk@gmail.com</span>
+        </div>
+          
+      </div>
+    </div>
   )
 }
 
